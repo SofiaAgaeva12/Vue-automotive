71b82a5a0b94b9b7765e14017e9a3e1b97b2e306
SofiaAgaeva12 <sofiaagaeva45744@gmail.com>
2022-12-02 11:40:30 +0700

    registration and authorization

--- vue-project/src/components/AppLogin.vue
+++ vue-project/src/components/AppLogin.vue
@@ -5,16 +5,19 @@
         <div class="row">
           <form>
             <div class="form-floating mb-3">
-              <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+              <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" v-model="model['email']">
               <label for="floatingInput">Email</label>
             </div>
+            <p class="link-danger">{{ errors["email"] }}</p>
             <div class="form-floating mb-3">
-              <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+              <input type="password" class="form-control" id="floatingPassword" placeholder="Password" v-model="model['password']">
               <label for="floatingPassword">Password</label>
             </div>
-
-            <button class="w-100 btn btn-lg btn-primary mb-3" type="submit">Войти</button>
-            <button class="w-100 btn btn-lg btn-outline-info" type="submit">Назад</button>
+            <p class="link-danger">{{ errors["password"] }}</p>
+            <button class="w-100 btn btn-lg btn-primary mb-3" type="submit" @click.prevent="login">Войти</button>
+            <router-link to="/">
+              <button class="w-100 btn btn-lg btn-outline-info" type="submit">Назад</button>
+            </router-link>
           </form>
         </div>
 
@@ -24,11 +27,35 @@
 </template>
 
 <script>
+import axios from "axios";
+import router from "../router";
+const API_URL = 'https://jurapro.bhuser.ru/api-samohod';
 export default {
-  name: "AppLogin"
+  name: "AppLogin",
+  data: () => {
+    return {
+      model: {
+        "email": "",
+        "password": "",
+      },
+      click: false,
+      errors: [],
+      per: [],
+    }
+  },
+  methods: {
+    async login() {
+      await axios.post(`${API_URL}/login`, this.model).then((res) => {
+
+        this.per = res.data;
+        router.push('/');
+        console.log(res)
+      }).catch((err) => {
+        console.log(err)
+        this.errors = err
+      })
+    }
+  },
 }
+
 </script>
-
-<style scoped>
-
-</style>
--- vue-project/src/components/AppRegister.vue
+++ vue-project/src/components/AppRegister.vue
@@ -6,21 +6,27 @@
           <form>
             <h1 class="h3 mb-3 fw-normal">Пожалуйста заполните все поля</h1>
             <div class="form-floating mb-3">
-              <input type="text" class="form-control" id="floatingFio" placeholder="ФИО" v-model="model.fio">
+              <input type="text" class="form-control" id="floatingFio" placeholder="ФИО" v-model="model['fio']">
               <label for="floatingFio">ФИО</label>
             </div>
+            <p class="link-danger">{{ errors["fio"] }}</p>
             <div class="form-floating mb-3">
-              <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" v-model="model.email">
+              <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"
+                     v-model="model['email']">
               <label for="floatingInput">Email</label>
             </div>
+            <p class="link-danger">{{ errors["email"] }}</p>
             <div class="form-floating mb-3">
-              <input type="password" class="form-control" id="floatingPassword" placeholder="Password" v-model="model.password">
+              <input type="password" class="form-control" id="floatingPassword" placeholder="Password"
+                     v-model="model['password']">
               <label for="floatingPassword">Password</label>
-              <span v-for="error in errors" :key="error" class="error">{{ error.password }}</span>
             </div>
-
-            <button class="w-100 btn btn-lg btn-primary mb-3" type="submit" v-on:click="signup">Зарегистрироваться</button>
+            <p class="link-danger">{{ errors["password"] }}</p>
+            <button class="w-100 btn btn-lg btn-primary mb-3" type="submit" @click.prevent="signup">Зарегистрироваться
+            </button>
+            <router-link to="/">
             <button class="w-100 btn btn-lg btn-outline-info" type="submit">Назад</button>
+            </router-link>
           </form>
         </div>
 
@@ -32,36 +38,37 @@
 <script>
 import axios from "axios";
 import router from "../router";
-const API_URL = 'https://jurapro.bhuser.ru/api-samohod/';
+
+const API_URL = 'https://jurapro.bhuser.ru/api-samohod';
 
 export default {
   name: "AppRegister",
-
-data: () => {
-  return {
-    model: {
-      fio: '',
-      email: '',
-      password: '',
-    },
-    click: false,
-    errors: [],
-    per: [],
+  data: () => {
+    return {
+      model: {
+        "fio": "",
+        "email": "",
+        "password": "",
+      },
+      click: false,
+      errors: [],
+      per: [],
+    }
+  },
+  props: {
+    isLogin: Boolean,
+  },
+  methods: {
+    async signup() {
+      await axios.post(`${API_URL}/signup`, this.model).then((res) => {
+        console.log(res)
+        router.push('/login')
+      }).catch((err) => {
+        console.log(err.response.data.warning.warnings)
+        this.errors = err.response.data.warning.warnings
+      })
+    }
   }
-},
-    props: {
-  isLogin: Boolean,
-},
-methods: {
-  async signup() {
-    await axios.post(`${API_URL}/signup`, this.model ).then((res) => {
-      console.log(res)
-      router.push('/login')
-    }).catch((err) => {
-      console.log(err.response.data.error)
-      this.errors = err.response.data.error
-    })
-  }
-}
 }
 </script>
+
